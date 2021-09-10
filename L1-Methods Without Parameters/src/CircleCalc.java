325f7b4031b1e9379c9a35cc5d99494c53331c7e
IvanG422 <guerriva000@warren.k12.in.us>
2021-09-10 11:45:23 -0400

    djyfgkfh

--- L1-Methods Without Parameters/src/CircleCalc.java
+++ L1-Methods Without Parameters/src/CircleCalc.java
@@ -3,15 +3,21 @@ public class CircleCalc {
 
     public static void main(String[] args) {
 
+        area();
         //call the area method
-
+        circumference();
         //call the circumference method
 
     }
 
+    public static void area(){
+        double area =
+    }
     //write a method to calculate a circle's area
 
+    public static void circumference(){
 
+    }
     //write a method to calculate a circle's circumference
 
 }
