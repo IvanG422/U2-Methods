06565a455d90e845c2fe6ac7eeb2d4ec1670bef1
IvanG422 <guerriva000@warren.k12.in.us>
2021-09-10 11:53:22 -0400

    New

--- L2-Methods with Parameters/src/CircleCalc.java
+++ L2-Methods with Parameters/src/CircleCalc.java
@@ -1,15 +1,40 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+
 public class CircleCalc {
 
 
     public static void main(String[] args) {
 
-        //create and run the program
+
+        //call the area method
+        area();
+        //call the circumference method
+        circumference();
+    }
+
+    //write a method to calculate a circle's area
+    public static void area() {
+        DecimalFormat round = new DecimalFormat("#.##");
+
+        double radius = Double.parseDouble(JOptionPane.showInputDialog("What is the radius of the circle?"));
+
+        double area = Math.PI * Math.pow(radius, 2);
+
+        JOptionPane.showMessageDialog(null, "The area is " + round.format(area));
+
 
     }
 
-    //re-write the method to calculate a circle's area using parameters
+    //write a method to calculate a circle's circumference
+    public static void circumference() {
+        DecimalFormat round = new DecimalFormat("#.##");
 
+        double radius = Double.parseDouble(JOptionPane.showInputDialog("What is the radius of the circle?"));
 
-    //re-write the method to calculate a circle's perimeter using parameters
+        double circumference = 2 * Math.PI * radius;
 
+        JOptionPane.showMessageDialog(null, "The circumference is " + round.format(circumference));
+    }
 }
