df39517b4e425d1d2a1a368041dbdb16439e63d9
IvanG422 <guerriva000@warren.k12.in.us>
2021-09-16 11:51:36 -0400

    L3 Return Methods

--- L3-Return Methods/src/CircleCalc.java
+++ L3-Return Methods/src/CircleCalc.java
@@ -1,15 +1,60 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class CircleCalc {
 
+    /*
+    1. Get input - get the radius
+    2. Calculate Area
+    3. Calculate Circumference
+    4. Display Results
+     */
 
     public static void main(String[] args) {
 
         //create and run the program
+        // 1.
+        double radius = getInput("Please enter the radius");
+
+        //2.
+        double area = area(radius);
+
+        //3.
+        double circumference = circumference(radius);
+
+        //4.
+        JOptionPane.showMessageDialog(null, displayResults(area, circumference));
 
     }
 
-    //re-write the method to calculate a circle's area using parameters and return statement
+    //Method to get input from user
+    public static double getInput(String message){
 
+        return Double.parseDouble(JOptionPane.showInputDialog(message));
+
+    }
+
+
+    //re-write the method to calculate a circle's area using parameters and return statement
+    public static double area(double radius){
+
+        return Math.pow(radius, 2) * Math.PI;
+
+    }
 
     //re-write the method to calculate a circle's perimeter using parameters and return statement
+    public static double circumference(double radius){
+
+        return 2 * Math.PI * radius;
+    }
+
+    public static String displayResults(double area, double circumference){
+
+        DecimalFormat round = new DecimalFormat("#.00");
+        String message = "The area is: " + round.format(area);
+        message += "\nThe circumference is: " + round.format(circumference);
+
+        return message;
+    }
 
 }
--- L3-Return Methods/src/TrianglePerimeter.java
+++ L3-Return Methods/src/TrianglePerimeter.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class TrianglePerimeter {
 
     /*
@@ -10,7 +13,52 @@ public class TrianglePerimeter {
     for calculating the perimeter of a triangle.
      */
 
-    public static void main(String[] args) {
+    /*
+    1. Get input for the coordinates, x1, y1, x2, y2, x3, y3
+    2. Calculate the distances between cords = 1,2; 2, 3; 3,1
+    3. Calculate the perimeter of the triangle
+    4. Display results - round to 2 decimals
+    */
 
+    public static void main(String[] args) {
+        int x1 = getInput("Point 1, x-coordinate:");
+        int y1 = getInput("Point 1, y-coordinate:");
+
+        int x2 = getInput("Point 2, x-coordinate:");
+        int y2 = getInput("Point 2, y-coordinate:");
+
+        int x3 = getInput("Point 3, x-coordinate:");
+        int y3 = getInput("Point 3, y-coordinate:");
+
+        double distance1 = distance(x1, x2, y1, y2);
+        double distance2 = distance(x2, x3, y2, y3);
+        double distance3 = distance(x3, x1, y3, y1);
+
+        double perimeter = triPerimeter(distance1, distance2, distance3);
+
+        outputResults(perimeter);
+
+        System.exit(0);
     }
+    //Input Method
+    public static int getInput(String message){
+        return Integer.parseInt(JOptionPane.showInputDialog(message));
+    }
+
+    //Distance Method
+    public static double distance(int x1, int x2, int y1, int y2){
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    public static double triPerimeter(double side1, double side2, double side3){
+        return side1 + side2 + side3;
+    }
+
+    //Output
+    public static void outputResults(double perimeter){
+        DecimalFormat round = new DecimalFormat("#,###.00");
+
+        JOptionPane.showMessageDialog(null, "The perimeter is " + round.format(perimeter));
+    }
+
 }
