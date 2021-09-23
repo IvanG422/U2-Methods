e5385f39953fb05c205cd0b2e71925bfb571d68d
IvanG422 <guerriva000@warren.k12.in.us>
2021-09-23 12:13:14 -0400

    2.4 Practice Problems

--- L4-Strings/src/LastLetterWord.java
+++ L4-Strings/src/LastLetterWord.java
@@ -1,18 +1,36 @@
+import javax.swing.*;
+
 public class LastLetterWord {
 
     /*
     Create a program that takes the last 3 letters of each String
     and outputs them as one string.
-
     Use a method to find and return the last three letters and a string parameter.
-
     Ex:  Goodbye, Hello, Morning -> byelloing
     */
 
+    /*
+    1. Get Input: three words
+    2. Isolate the last three letters of any word
+    3. Combine #2 of all three words
+     */
+
     public static void main(String[] args) {
 
+        String word1 = JOptionPane.showInputDialog("Enter Word 1: ");
+        String word2 = JOptionPane.showInputDialog("Enter Word 2: ");
+        String word3 = JOptionPane.showInputDialog("Enter Word 3: ");
 
+        String output = lastThree(word1) + lastThree(word2) + lastThree(word3);
 
+        JOptionPane.showMessageDialog(null, output);
+
+        System.exit(0);
+
+    }
+
+    public static String lastThree(String word){
+        return word.substring(word.length()-3);
     }
 
 }
--- L4-Strings/src/SplitPs.java
+++ L4-Strings/src/SplitPs.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class SplitPs {
 
     /*
@@ -5,4 +7,26 @@ public class SplitPs {
 
     "Hippopotamus" -> ppop
      */
+
+    /*
+    1. Get Input - a word hopefully with a p and letters
+    2. Isolate the p
+    3. Isolate the three letters AFTER the p
+    4. Output
+     */
+
+    public static void main(String[] args) {
+        String word = JOptionPane.showInputDialog("Enter a word that has a p, and three letters after it: ");
+        JOptionPane.showMessageDialog(null, splitP(word));
+
+        System.exit(0);
+    }
+
+    public static String splitP(String word){
+        int pIndex = word.indexOf('p');
+
+        String split = word.substring(pIndex + 1, pIndex + 4);
+
+        return split;
+    }
 }
